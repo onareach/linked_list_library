175eca3047d4e7204d7ceda07bcab56723abae39
David Long <david@aucklandresearch.com>
2024-04-07 21:41:53 -0700

    Added more diagnostic code and comments to struct_node_syntax_conventions.c and added a compiled version of the file.

--- struct_node_syntax_conventions.c
+++ struct_node_syntax_conventions.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 // People have asked why the word "struct node" appears twice in the struct declaration below – both
 // before the {} and inside the {}. From what I can gather, the "struct node" inside the {}
@@ -13,11 +16,18 @@
 // because it is ignorantly copied or, better, because it makes the code backward-compatible with C.
 typedef struct node {
     int value;
-    stuct node * next;
-} Node
+    char alpha;
+    struct node * next;
+} Node;
+
+// Print functions:
+void print_list_full(Node *head); 
+
+// Insert and delete Node functions:
+Node *insert_full_head(Node *head, int new_value, char new_alpha);
 
 
-void main()
+int main()
 {
     // In the declaration below, the Node (struct) datatype is followed by a declaration of the 
     // name of a pointer to its head node. Later, if you want to operate on the list, you
@@ -31,5 +41,57 @@ void main()
     // but that would involve three characters ( < and = and > )instead of one (=).
     Node *list1_head = NULL;
 
+    printf("\nThe address of the pointer list1_head itself: %p\n", &list1_head);
+    printf("The value that the pointer list1_head currently holds: %p\n", list1_head);
+
+    list1_head = insert_full_head(list1_head, 1, 'a');
+  
+    printf("\nList of Nodes after inserting a head:\n");
+    print_list_full(list1_head);
+
+    printf("The 0x0 value in the .next value of the Nodes list, shows there \n");
+    printf("is no next node.\n");
+
+    printf("\nThe address of the pointer list1_head itself: %p\n", &list1_head);
+    printf("The value that the pointer list1_head currently holds: %p\n", list1_head);
+    
+    printf("\nNow that there is a head node, the list1_head pointer now holds a value.\n");
+    printf("This latter value will change as the head node is replaced, but the memory \n");
+    printf("address of the pointer itself will remain fixed as long as the list exists.\n");
+
     return 0;
+
 }
+
+// Print functions:
+void print_list_full(Node *head)
+{
+  Node *current;
+  current = head;
+  int i = 0;
+  printf("List of integers, characters, and next node pointer:\n");
+
+  while (current != NULL)
+  {
+    printf("Node %d: %3d, %c, %p\n", i, current->value,
+        current->alpha, current->next);
+    i++;
+    current = current->next;
+  }
+  printf("\n");
+}
+
+// Insert functions:
+Node *insert_full_head(Node *head, int new_value, char new_alpha)
+{
+  Node *new_node = calloc(1, sizeof(Node));
+  new_node->value = new_value;
+  new_node->alpha = new_alpha;
+
+  if (head == NULL) return new_node;        
+  else                                     
+  {                                       
+    new_node->next = head;                  
+    return new_node;                        
+  }                                         
+}                                          
